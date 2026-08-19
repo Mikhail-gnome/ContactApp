0193216a613e20f746e4a9136a7ff195ed1f2985
Mikhail <ivanov-mihail@inbox.ru>
2026-08-20 00:38:33 +0300

    Spring initialized  with lombok  spring web

---
+++ src/main/java/Mikhail_gnome/com/github/contactApp/ContactAppApplication.java
@@ -0,0 +1,13 @@
+package Mikhail_gnome.com.github.contactApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Mikhail_gnome/com/github/contactApp/ContactAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Mikhail_gnome.com.github.contactApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
