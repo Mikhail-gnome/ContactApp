d3d08bedc223a435dcd6c818359aca08b2fc4ebf
Mikhail <ivanov-mihail@inbox.ru>
2026-09-08 23:00:17 +0300

    A phone number validation based on regular expressions has been added to CreateContactDto

--- src/main/java/Mikhail_gnome/com/github/contactApp/model/dto/CreateContactDto.java
+++ src/main/java/Mikhail_gnome/com/github/contactApp/model/dto/CreateContactDto.java
@@ -3,6 +3,7 @@ package Mikhail_gnome.com.github.contactApp.model.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Getter
@@ -15,6 +16,7 @@ public class CreateContactDto {
         private String firstName;
         @NotEmpty(message = "Имя не может быть пустым")
         private String lastName;
+        @Pattern(regexp = "\\+7\\(\\d{3}\\)\\d{3} \\d{2} \\d{2}", message = "Неверный формат телефона")
         private String telephone;
         @Email(message = "Некорректный email")
         private String email;
