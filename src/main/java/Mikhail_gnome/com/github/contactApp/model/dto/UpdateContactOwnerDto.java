5be71fc1033f2c298dec0059a3c1bce1b5409a7b
Mikhail <ivanov-mihail@inbox.ru>
2026-09-10 00:22:30 +0300

    add class UpdateContactOwnerDto and validation

--- src/main/java/Mikhail_gnome/com/github/contactApp/model/dto/UpdateContactOwnerDto.java
+++ src/main/java/Mikhail_gnome/com/github/contactApp/model/dto/UpdateContactOwnerDto.java
@@ -1,4 +1,27 @@
 package Mikhail_gnome.com.github.contactApp.model.dto;
 
+import Mikhail_gnome.com.github.contactApp.model.enums.AppRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class UpdateContactOwnerDto {
+
+    @NotBlank(message = "ID обязателен")
+    private String id;
+
+    @Size(min = 6, max = 20, message = "Имя пользователя должно быть от 6 до 20 символов")
+    private String username;
+
+    @NotNull(message = "Значение null недопустимо")
+    private String description;
+
+    @NotBlank(message = "Поле не может быть пустым")
+    @Email(message = "Некорректный Email")
+    private String email;
+
+    @Size(min = 2, max = 50, message = "Это поле должно содержать от 2 до  50 символов")
+    private String fullName;
+
 }
